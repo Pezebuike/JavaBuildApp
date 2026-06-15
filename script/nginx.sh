1ab3e3a504ddad5176242fdcdb07d6dc92de6c89
Ezebuike Michael <ezebuikemichael@yahoo.com>
2026-06-15 07:00:28 +0100

    fix: script.sh

--- script/nginx.sh
+++ script/nginx.sh
@@ -3,7 +3,11 @@ set -e
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt-get update -y
-sudo apt-get install -y nginx
+sudo -E apt-get update -y
+sudo -E apt-get install -y \
+  -o Dpkg::Options::="--force-confdef" \
+  -o Dpkg::Options::="--force-confold" \
+  nginx
+
 sudo systemctl enable nginx 2>&1
 sudo systemctl start nginx 2>&1
